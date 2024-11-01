b2438fedb872c3b69a47d4c1cdfc64ef0e8c1b99
Matvey Kostukovsky <email@matveycodes.com>
2024-10-31 18:46:46 -0700

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Matvey Kostukovsky on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
